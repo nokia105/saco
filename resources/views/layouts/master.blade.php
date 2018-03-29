edac4039697ded06b7059da195d1c29e76a44bfc
nokia105 <31007586+nokia105@users.noreply.github.com>
2018-03-29 17:53:40 +0300

    master
    bb

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -144,8 +144,8 @@
     <!-- Content Header (Page header) -->
     <section class="content-header">
       <h1>
-        Dashboard
-        <small>Control panel</small>
+        SaCoSS
+        <small></small>
       </h1>
       <ol class="breadcrumb">
         <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
