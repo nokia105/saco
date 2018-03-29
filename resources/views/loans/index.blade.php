f7d059a8d82e89331fb14942118b1189e8a5d46c
chrisoemma <chrissoemma@gmail.com>
2018-03-29 14:42:07 +0300

    loan master template
    loan master template

--- resources/views/loans/index.blade.php
+++ resources/views/loans/index.blade.php
@@ -1,136 +1,39 @@
     <!-- Main content -->
 
-
-       @extends('layouts.master')
-      @section('content')
-    <section class="content">
-      <div class="row box box-info" style="background-color:#fff; ">
-        <!-- left column -->
-        <div class="col-md-6 padding-0" >
-          <!-- general form elements -->
-          <div class="">
-            <div class="box-header with-border">
-              <h3 class="box-title">Quick Example</h3>
+       @extends('loans.master')
+      @section('userworkspace')
+     <div class="col-md-12">
+          <div class="box col-md-12">
+            <div class="box-header">
+              <h3 class="box-title">Loan List</h3>
             </div>
             <!-- /.box-header -->
-            <!-- form start -->
-            <form role="form">
-              <div class="box-body">
-                <div class="form-group">
-                  <label for="exampleInputEmail1">Email address</label>
-                  <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
-                </div>
-                <div class="form-group">
-                  <label for="exampleInputPassword1">Password</label>
-                  <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-                </div>
-                <div class="form-group">
-                  <label for="exampleInputFile">File input</label>
-                  <input type="file" id="exampleInputFile">
-
-                  <p class="help-block">Example block-level help text here.</p>
-                </div>
-                <div class="checkbox">
-                  <label>
-                    <input type="checkbox"> Check me out
-                  </label>
-                </div>
-              </div>
-              <!-- /.box-body -->
-
-              <div class="box-footer">
-                <button type="submit" class="btn btn-primary">Submit</button>
-              </div>
-            </form>
-          </div>
-          <!-- /.box -->
-
-          <!-- Form Element sizes -->
-          <!-- <div class="box box-success">
-            <div class="box-header with-border">
-              <h3 class="box-title">Different Height</h3>
-            </div>
             <div class="box-body">
-              <input class="form-control input-lg" type="text" placeholder=".input-lg">
-              <br>
-              <input class="form-control" type="text" placeholder="Default input">
-              <br>
-              <input class="form-control input-sm" type="text" placeholder=".input-sm">
+              <table id="example1" class="table table-bordered table-striped">
+                <thead>
+                <tr>
+                  <th>Rendering engine</th>
+                  <th>Browser</th>
+                  <th>Platform(s)</th>
+                  <th>Engine version</th>
+                  <th>CSS grade</th>
+                </tr>
+                </thead>
+                <tbody>
+                
+                <tr>
+                  <td>Other browsers</td>
+                  <td>All others</td>
+                  <td>-</td>
+                  <td>-</td>
+                  <td>U</td>
+                </tr>
+                </tbody>
+               
+              </table>
             </div>
-            
-          </div> -->
-          <!-- /.box -->
-
-<!--           <div class="box box-danger">
-  <div class="box-header with-border">
-    <h3 class="box-title">Different Width</h3>
-  </div>
-  <div class="box-body">
-    <div class="row">
-      <div class="col-xs-3">
-        <input type="text" class="form-control" placeholder=".col-xs-3">
-      </div>
-      <div class="col-xs-4">
-        <input type="text" class="form-control" placeholder=".col-xs-4">
-      </div>
-      <div class="col-xs-5">
-        <input type="text" class="form-control" placeholder=".col-xs-5">
-      </div>
-    </div>
-  </div>
-  
-</div> -->
-          <!-- /.box -->
-
-
-
-        </div>
-        <!--/.col (left) -->
-        <!-- right column -->
-        <div class="col-md-6 padding-0">
-          <!-- Horizontal Form -->
-          <div class="">
-            <div class="box-header with-border">
-              <h3 class="box-title">Horizontal Form</h3>
-            </div>
-            <!-- /.box-header -->
-            <!-- form start -->
-  <form role="form">
-              <div class="box-body">
-                <div class="form-group">
-                  <label for="exampleInputEmail1">Email address</label>
-                  <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
-                </div>
-                <div class="form-group">
-                  <label for="exampleInputPassword1">Password</label>
-                  <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-                </div>
-                <div class="form-group">
-                  <label for="exampleInputFile">File input</label>
-                  <input type="file" id="exampleInputFile">
-
-                  <p class="help-block">Example block-level help text here.</p>
-                </div>
-                <div class="checkbox">
-                  <label>
-                    <input type="checkbox"> Check me out
-                  </label>
-                </div>
-              </div>
-              <!-- /.box-body -->
-
-              <div class="box-footer">
-                <button type="submit" class="btn btn-primary">Submit</button>
-              </div>
-            </form>
+            <!-- /.box-body -->
           </div>
-       
-          <!-- /.box -->
-         
           <!-- /.box -->
         </div>
-        <!--/.col (right) -->
-      </div>
-      <!-- /.row -->
-    </section>
     @endsection
